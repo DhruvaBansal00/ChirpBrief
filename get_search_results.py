1162bbf0c555982e2b31af2da4b0193a21e60f0b
Dhruva Bansal <dhruva.bansal00@gmail.com>
2023-03-11 22:20:05 -0800

    Search using selenium

---
+++ get_search_results.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def return_search_results(query_text):
+    options = webdriver.ChromeOptions()
+    options.add_argument(r"user-data-dir=chrome_data/")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver.get("https://twitter.com/explore")
+
+    form = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.TAG_NAME, "input"))
+    )
+    form.send_keys(query_text)
+    form.submit()
+    return driver
+
+
+driver = return_search_results("Dhruva Bansal?")
+print(driver.page_source)
--- try.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-options = webdriver.ChromeOptions()
-options.add_argument(r"user-data-dir=chrome_data/")
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-import pdb
-
-pdb.set_trace()
