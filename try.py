2ab9e94c5a373469a642ef6ab257128ed62d4716
Dhruva Bansal <dhruva.bansal00@gmail.com>
2023-03-11 19:22:15 -0800

    gitignore and sign in code

--- try.py
+++ try.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
+options.add_argument(r"user-data-dir=chrome_data/")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+import pdb
+
+pdb.set_trace()
