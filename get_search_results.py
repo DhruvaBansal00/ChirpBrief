bdae48ce8c766ac00c373a6e6c8a6bf67c7d6f68
Dhruva Bansal <dhruva.bansal00@gmail.com>
2023-03-14 00:56:16 -0700

    Summary and bio generation methods ready

--- get_search_results.py
+++ get_search_results.py
@@ -1,3 +1,17 @@
+import getpass
+from helm.common.authentication import Authentication
+from helm.common.perspective_api_request import (
+    PerspectiveAPIRequest,
+    PerspectiveAPIRequestResult,
+)
+from helm.common.request import Request, RequestResult
+from helm.common.tokenization_request import (
+    TokenizationRequest,
+    TokenizationRequestResult,
+)
+from helm.proxy.accounts import Account
+from helm.proxy.services.remote_service import RemoteService
+
 import time
 from urllib.parse import quote
 import openai
@@ -11,7 +25,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 options = webdriver.ChromeOptions()
 options.add_argument(r"user-data-dir=chrome_data/")
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-openai.api_key = "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"
+api_key = "iMNfpmV6ziCZvzCe50gyjks7QVMEuLBw"
+auth = Authentication(api_key=api_key)
+service = RemoteService("https://crfm-models.stanford.edu")
 
 
 def return_search_results(query_text):
@@ -30,11 +46,12 @@ def find_tweets(driver, num_loops=10):
         bs = BeautifulSoup(driver.page_source)
         articles = bs.find_all("article")
         for article in articles:
-            l = article.find("div", {"data-testid": "tweetText"}).find_all("span")
-            l = [i.text for i in l]
-            tweet = "".join(l)
-            if tweet not in all_tweets:
-                all_tweets.append(tweet)
+            if article.find("div", {"data-testid": "tweetText"}) is not None:
+                l = article.find("div", {"data-testid": "tweetText"}).find_all("span")
+                l = [i.text for i in l]
+                tweet = "".join(l)
+                if tweet not in all_tweets:
+                    all_tweets.append(tweet)
         driver.execute_script(f"window.scrollBy(0,5*window.innerHeight)")
     return all_tweets
 
@@ -87,66 +104,43 @@ def get_all_tweets_for_user(user_name):
     return find_tweets(driver)
 
 
-def generate_tweet_summary(tweets):
+def generate_tweet_summary(query, tweets):
     tweet_list_concat = " ".join(tweets)
-    prompt = f"Generate a 5 line summary of the following tweets: {tweet_list_concat}"
-    return (
-        openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            max_tokens=512,
-            temperature=0,
-        )
-        .choices[0]
-        .text
-    )
+    prompt = f"Generate a 5 line summary of what people are saying on twitter about {query} based on the following tweets: {tweet_list_concat}. Summary: "
+    request = Request(model="openai/text-davinci-003", prompt=prompt, max_tokens=1024)
+    request_result = service.make_request(auth, request)
+    return request_result.completions[0].text.replace("\n", "")
 
 
 def generate_tweet_summary_for_user(username):
     tweets = get_all_tweets_for_user(username)
     tweet_list_concat = " ".join(tweets)
-    prompt = f"Generate a 5 line summary of the following tweets posted by {username}: {tweet_list_concat}"
-    return (
-        openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            max_tokens=512,
-            temperature=0,
-        )
-        .choices[0]
-        .text
-    )
+    prompt = f"Generate a 5 line summary about what {username} posts about and likes based on their following tweets: {tweet_list_concat}. Summary: "
+    request = Request(model="openai/text-davinci-003", prompt=prompt, max_tokens=1024)
+    request_result = service.make_request(auth, request)
+    return request_result.completions[0].text.replace("\n", "")
 
 
 def generate_bio(username):
     tweets = get_all_tweets_for_user(username)
     tweet_list_concat = " ".join(tweets)
-    prompt = f"Generate a twitter bio for {username} who has tweeted the following: {tweet_list_concat}"
-    return (
-        openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            max_tokens=512,
-            temperature=0,
-        )
-        .choices[0]
-        .text
-    )
+    prompt = f"Generate a twitter bio for {username} who has tweeted the following: {tweet_list_concat}. Twitter bio: "
+    request = Request(model="openai/text-davinci-003", prompt=prompt, max_tokens=1024)
+    request_result = service.make_request(auth, request)
+    return request_result.completions[0].text.replace("\n", "")
 
 
 def generate_trending_topic_summary(topic):
     tweets = get_all_tweets_for_trending_topic(topic)
-    return generate_tweet_summary(tweets)
+    return generate_tweet_summary(topic, tweets)
 
 
 def generate_query_summary(query_text):
     tweets = get_all_search_tweets(query_text)
-    return generate_tweet_summary(tweets)
+    return generate_tweet_summary(query_text, tweets)
 
 
-print(generate_query_summary("Silicon Valley Bank"))
-
-
-# all_topics = get_all_topics_from_trending()
-# print(get_all_tweets_for_trending_topic(all_topics[1]))
-# print(get_all_tweets_for_user("elonmusk"))
+# print(generate_query_summary("spacex"))
+# print(generate_tweet_summary_for_user("elonmusk"))
+# print(generate_bio("elonmusk"))
+# print(generate_trending_topic_summary("Klay"))
