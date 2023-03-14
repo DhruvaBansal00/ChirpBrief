9ee9f334d2ea8c1b612d738e269486db35dceaed
Rajas Bansal <rajasbansal1998@gmail.com>
2023-03-13 21:41:59 -0700

    Added find tweets

--- get_search_results.py
+++ get_search_results.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_argument(r"user-data-dir=chrome_data/")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 
 def return_search_results(query_text):
@@ -17,6 +21,21 @@ def return_search_results(query_text):
     form.send_keys(query_text)
     form.submit()
     return driver
+
+
+def find_tweets(driver, num_loops=10):
+    all_tweets = []
+    for _ in range(num_loops):
+        bs = BeautifulSoup(driver.page_source)
+        articles = bs.find_all("article")
+        for article in articles:
+            l = article.find_all("span")[4].parent.find_all("span")
+            l = [i.text for i in l]
+            tweet = "".join(l)
+            if tweet not in all_tweets:
+                all_tweets.append(tweet)
+        driver.execute_script(f"window.scrollBy(0,5*window.innerHeight)")
+    return all_tweets
 
 
 # driver = return_search_results("Dhruva Bansal?")
