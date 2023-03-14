dc8eb33b0ce77104e1f5211878c5a58c129db6de
Dhruva Bansal <dhruva.bansal00@gmail.com>
2023-03-14 01:21:44 -0700

    Connecting backend and frontend

--- interface.py
+++ interface.py
@@ -8,6 +8,11 @@ with gr.Blocks() as demo:
         search_input = gr.Textbox(label="Search input")
         search_output = gr.Textbox(label="Search summary")
         search_button = gr.Button("Search and Generate Summary")
+        search_button.click(
+            fn=generate_query_summary,
+            inputs=search_input,
+            outputs=search_output,
+        )
 
     with gr.Tab("Get top trending summary"):
         topics = get_all_topics_from_trending()
@@ -21,10 +26,20 @@ with gr.Blocks() as demo:
         username_input = gr.Textbox(label="Username to summarize")
         username_output = gr.Textbox(label="Summarized profile")
         user_summary_button = gr.Button("Generate summary for user")
+        user_summary_button.click(
+            fn=generate_tweet_summary_for_user,
+            inputs=username_input,
+            outputs=username_output,
+        )
 
     with gr.Tab("Generate User Bio"):
         bio_username_input = gr.Textbox(label="Username to summarize")
         bio_username_output = gr.Textbox(label="Generated Bio")
         generate_bio_button = gr.Button("Generate bio for user")
+        generate_bio_button.click(
+            fn=generate_bio,
+            inputs=bio_username_input,
+            outputs=bio_username_output,
+        )
 
 demo.launch()
